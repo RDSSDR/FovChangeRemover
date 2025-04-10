1c00776438f723d7a9fa5c915963454bef883cb4
RDSSDR <nhenson1031@gmail.com>
2025-04-09 22:57:20 -0400

    changed fov to be changeable in settings

--- FCR.cs
+++ FCR.cs
@@ -1,7 +1,10 @@
 using Landfall.Haste;
 using Landfall.Modding;
 using System.Reflection;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Localization;
+using Zorro.Settings;
 
 namespace FCR;
 
@@ -27,8 +30,19 @@ public class Program
             return;
         }
 
-        Type type = typeof(CameraMovement);
+        /*Type type = typeof(CameraMovement);
         FieldInfo field = type.GetField("baseFOV", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-        Camera.main.fieldOfView = (float)field.GetValue(self);
+        Camera.main.fieldOfView = (float)field.GetValue(self);*/
+        Camera.main.fieldOfView = GameHandler.Instance.SettingsHandler.GetSetting<FovSetting>().Value;
     }
+}
+
+[HasteSetting]
+public class FovSetting : FloatSetting, IExposedSetting
+{
+    public override void ApplyValue() => Debug.Log($"Mod apply value {Value}");
+    protected override float GetDefaultValue() => 120;
+    protected override float2 GetMinMaxValue() => new(0, 200);
+    public LocalizedString GetDisplayName() => new UnlocalizedString("FOV");
+    public string GetCategory() => SettingCategory.Graphics;
 }
